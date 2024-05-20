c5aaa4fc89da14e9390b10b0bb8df6b77983aa5b
Nikhil Anand <mail@nikhil.io>
2024-05-20 10:32:30 -0500

    OpenGraph

--- .scripts/build-and-deploy.sh
+++ .scripts/build-and-deploy.sh
@@ -4,7 +4,24 @@
 
 set -euo pipefail
 
-pnpm run clean
-parcel build
-cp src/assets/robots.txt dist/
+echo "Building"
+echo "------------------------------------------------------------------------"
+pnpm build
 
+echo "Compressing output"
+echo "------------------------------------------------------------------------"
+find dist/ -type f -exec gzip -9 "{}" \; -exec mv "{}.gz" "{}" \;
+
+echo "Syncing"
+echo "------------------------------------------------------------------------"
+aws s3 sync dist/ s3://nikhil.io/ --delete \
+  --cache-control max-age=315360000 \
+  --content-encoding gzip \
+  --profile nikhil.io
+
+echo "Invalidating"
+echo "------------------------------------------------------------------------"
+aws cloudfront create-invalidation \
+  --distribution-id E1YXRY7X6OHL2S \
+  --paths '/*' \
+  --profile nikhil.io
